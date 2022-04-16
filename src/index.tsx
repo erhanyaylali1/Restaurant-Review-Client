fbc068fef91f3be5fd132ef5f6685575d3cc98df
Erhan Yaylalı <32177766+erhanyaylali1@users.noreply.github.com>
2022-04-16 14:56:35 +0300

    i18n is configured, all packages are installed

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
+import "./utils/i18n";
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
